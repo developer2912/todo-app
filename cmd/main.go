cb848e44fc9686631b59651009f4d7019834f916
Andrey Dubov <developer2912@gmail.com>
2021-05-01 13:09:34 +0400

    Добавлена инициализация сущностей

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,15 @@ import (
 
 	"github.com/developer2912/todo-app"
 	"github.com/developer2912/todo-app/pkg/handler"
+	"github.com/developer2912/todo-app/pkg/repository"
+	"github.com/developer2912/todo-app/pkg/service"
 )
 
 func main() {
 
-	handlers := new(handler.Handler)
+	repositories := repository.NewRepository()
+	services := service.NewService(repositories)
+	handlers := handler.NewHandler(services)
 
 	server := new(todo.Server)
 	if err := server.Run("8080", handlers.InitRouts()); err != nil {
